2035109bd1403e7e333aaf08b7b14f178640be27
Divya Rajpurohit <divya297179@gmail.com>
2025-03-28 20:22:02 +0530

    Adding more function to the Filemanaging So user can add content

--- fille_manager.py
+++ fille_manager.py
@@ -9,3 +9,12 @@ def get_user_folder(username):
 if _name_ == "_main_":
     print("File management system initialized.")
 
+def create_file(username, file_name, content):
+    """Create a file and write content."""
+    folder_path = get_user_folder(username)
+    file_path = os.path.join(folder_path, file_name)
+
+    with open(file_path, "w") as f:
+        f.write(content)
+
+    print(f"✅ File '{file_name}' created for user '{username}'.")
